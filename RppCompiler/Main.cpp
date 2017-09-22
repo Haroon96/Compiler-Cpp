d00eee7eee35734eedf947c0c46da259aea028d4
Muhammad Haroon <m_haroon96@hotmail.com>
2017-09-23 00:16:44 +0500

    Added automata code, Lex v0.1

--- RppCompiler/Main.cpp
+++ RppCompiler/Main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include "lex/lex.h"
 using namespace std;
 
-int main() {
-	cout << "RppCompiler" << endl;
+int main(int argc, char *argv[]) {
+	/*
+	if (argc < 2) {
+		cerr << "Source file name not specified" << endl;
+		return 1;
+	}
+
+	ifstream src(argv[1]);*/
+
+	ifstream src("test.txt");
+
+	if (!src.is_open()) {
+		cerr << "Invalid source file name specified" << endl;
+		return 1;
+	}
+
+	std::vector<lex::TokenLexeme*> tlp = lex::analyze(&src);
+
+	for (int i = 0; i < tlp.size(); ++i) {
+		cout << tlp[i]->token << " " << tlp[i]->lexeme << endl;
+	}
+
+	src.close();
 	return 0;
 }
